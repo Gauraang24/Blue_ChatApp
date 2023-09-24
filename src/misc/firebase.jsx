12ecef11ec57a8fabbdb2fd3be3c14c190b82333
Gauraang24 <garryappdroid@gmail.com>
2023-09-25 03:36:25 +0530

    installed starter files from the chat_app_starter, and initialize firebase project

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,12 @@
+import firebase from "firebase/app"
+
+const config = {
+    apiKey: "AIzaSyAs5RFZqZ9z7XmI6WXYXfaVRhGAwzxIKzo",
+  authDomain: "blue-aae75.firebaseapp.com",
+  projectId: "blue-aae75",
+  storageBucket: "blue-aae75.appspot.com",
+  messagingSenderId: "610492602230",
+  appId: "1:610492602230:web:ef2e7c29df261a75db0c57"
+}
+
+firebase.initializeApp(config)
