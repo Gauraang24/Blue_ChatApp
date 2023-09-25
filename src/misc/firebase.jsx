9090fd053187ea946f5aab97349d40322bae78c0
Gauraang24 <garryappdroid@gmail.com>
2023-09-25 20:26:35 +0530

    added signIN page and signIn with Facebook/Google

--- src/misc/firebase.jsx
+++ src/misc/firebase.jsx
@@ -1,5 +1,6 @@
 import firebase from "firebase/app"
-
+import "firebase/auth"
+import "firebase/database"
 const config = {
     apiKey: "AIzaSyAs5RFZqZ9z7XmI6WXYXfaVRhGAwzxIKzo",
   authDomain: "blue-aae75.firebaseapp.com",
@@ -10,3 +11,5 @@ const config = {
 }
 
 const app = firebase.initializeApp(config)
+export const auth = app.auth()
+export const database = app.database()
