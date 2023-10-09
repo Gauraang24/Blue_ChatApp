0294fbf7130b979b9fac5c0adb3d2e2a75ed4727
Gauraang24 <garryappdroid@gmail.com>
2023-10-09 19:11:00 +0530

    created layout for home page and created chat page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Col, Grid, Row } from 'rsuite'
+import SideBar from '../../components/SideBar'
+import { RoomsProvider } from '../../context/rooms.context'
+import { Switch, Route, useRouteMatch } from 'react-router'
+import Chat from './Chat'
+import { useMediaQuery } from '../../misc/customHooks'
+
+const Home = () => {
+
+
+  const isDesktop = useMediaQuery('(min-width: 992px)')
+
+  const { isExact } = useRouteMatch()
+  // console.log("isExact value", isExact)
+
+
+  const canRenderSidebar = isDesktop || isExact
+
+  return (
+    <RoomsProvider>
+      <Grid fluid className='h-100'>
+        <Row className='h-100'>
+          {canRenderSidebar && <Col xs={24} md={8} className='h-100'>
+            <SideBar />
+          </Col>
+          }
+
+
+          <Switch>
+            <Route exact path="/chat/:chatId">
+              <Col xs={24} md={16} className='h-100'>
+                <Chat />
+              </Col>
+
+            </Route>
+            <Route>
+              {isDesktop && <Col xs={24} md={16} className='h-100' >
+                <h6 className='text-center mt-page'>Please Select Chat</h6></Col>}
+            </Route>
+          </Switch>
+        </Row>
+      </Grid>
+    </RoomsProvider>
+  )
+}
+
+export default Home
